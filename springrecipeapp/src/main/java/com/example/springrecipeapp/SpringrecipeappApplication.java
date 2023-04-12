dfb86637f6e5ad987262127d25e5b1ddbf19ba41
alec <alec.victory@intimetec.com>
2023-04-12 09:56:18 -0600

    initial commit

---
+++ springrecipeapp/src/main/java/com/example/springrecipeapp/SpringrecipeappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrecipeappApplication.class, args);
+    }
+
+}
