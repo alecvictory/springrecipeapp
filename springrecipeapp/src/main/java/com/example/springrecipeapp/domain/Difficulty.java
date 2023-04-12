94f9e0036b089c2064d4d8801e3fba782383e9ec
alec <alec.victory@intimetec.com>
2023-04-12 11:22:23 -0600

    added DB table settings & Repo interfaces

---
+++ springrecipeapp/src/main/java/com/example/springrecipeapp/domain/Difficulty.java
@@ -0,0 +1,4 @@
+package com.example.springrecipeapp.domain;
+
+public enum Difficulty {
+}
---
+++ springrecipeapp/src/main/java/com/example/springrecipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.springrecipeapp.repositories;
+
+import com.example.springrecipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
